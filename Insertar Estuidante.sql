2976c43b9921463d4cae414d5674268cb35d6865
Samuzarter <samuelquirozrincon@gmail.com>
2025-04-22 18:59:17 -0500

    Se agrega la colection de Postman y el Query de SQL con el estudiante

---
+++ Insertar Estuidante.sql
@@ -0,0 +1,8 @@
+USE DBExamen3;
+GO
+
+SELECT * FROM Estudiante
+SELECT * FROM Matricula
+
+INSERT INTO Estudiante(idEstudiante,Documento,NombreCompleto,Usuario,Clave)
+VALUES (1,'12345','Pepito perez','Pepito*PErez','123*')
